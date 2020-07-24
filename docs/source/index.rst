174b07b331e832ee49134ed43a617b8c48919afe
huangbaochen <huangbaochenwo@live.com>
2020-07-24 16:20:30 +0800

    remove cov-dir

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. pyTynETL documentation master file, created by
+   sphinx-quickstart on Fri Jul 24 16:19:12 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to pyTynETL's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- tests/test_etl_dag.py
+++ tests/test_etl_dag.py
@@ -1,6 +1,4 @@
 from pytynetl.etl_dag import (
-    ETLNode,
-    ETLDAG,
     node_,
     etl_dag_list,
 )
@@ -27,6 +25,30 @@ def test_dag_operator():
     print(mul2.task_name)
     print(dag.nodes)
     print(dag)
+
+    assert (add1 in dag)
+    assert (mul2 in dag)
+    assert (pow2 in dag)
+    assert dag['multiple'](2) == 4
+    assert dag['add1'](1) == 2
+    assert dag['pow2'](1) == 1
+
+def test_generate_dag_from_list():
+    dag = etl_dag_list()
+
+    @node_
+    def add1(x):
+        return x + 1
+
+    @node_(task_name="multiple")
+    def mul2(x):
+        return x * 2
+    
+    @node_
+    def pow2(x):
+        return x ** 2
+    
+    etl_dag_list(add1, mul2, pow2)
 
     assert (add1 in dag)
     assert (mul2 in dag)
